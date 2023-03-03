89339348e9e0aeb563f4dec7f5fdc0e3bd96cd39
Markus Wikman <Wikman.markus@gmail.com>
2023-03-03 14:47:24 +0100

    Added class band with props

---
+++ BandsMvc/models/Band.cs
@@ -0,0 +1,9 @@
+﻿namespace BandsMvc.Models
+{
+    public class Band
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public string Description { get; set; }
+    }
+}
