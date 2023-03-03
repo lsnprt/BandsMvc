34916676f7850db48a87c5b26cb1fd63cb899d75
Alessandro Peretti <115103372+lsnprt@users.noreply.github.com>
2023-03-03 14:39:56 +0100

    Added controller class

---
+++ BandsMvc/Controllers/BandsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BandsMvc.Controllers
+{
+    public class BandsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
