e77dc936668fe7250a6fc064a3a4ef69403709bb
YuriaTateishi <YuriaTateishi@AWA5CD2116DD1>
2023-03-03 15:23:37 +0100

    created controller implimented actions

--- BandsMvc/Controllers/BandsController.cs
+++ BandsMvc/Controllers/BandsController.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BandsMvc.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BandsMvc.Controllers
 {
     public class BandsController : Controller
     {
+        DataService dataService = new DataService();
+        [HttpGet("")]
+        [HttpGet("bands/index")]
+      
         public IActionResult Index()
         {
-            return View();
+            Band[] model = dataService.GetAll();
+            return View(model);
+        }
+
+        [HttpGet("bands/details/{id}")]
+        public IActionResult Details(int id) 
+        {
+            Band model = dataService.GetBandById(id);
+            return View(model);
         }
     }
 }
--- BandsMvc/models/Band.cs
+++ BandsMvc/models/Band.cs
@@ -5,5 +5,6 @@
         public string Name { get; set; }
         public int Id { get; set; }
         public string Description { get; set; }
+       
     }
 }
